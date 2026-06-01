ac2a031bdff6802205b6e0d06be18315b95abf4a
breakBlogHubVercel <breakgamer2601@gmail.com>
2026-06-01 21:54:02 +0700

    up

---
+++ src/main/java/com/re/fintechotpaop/FintechOtpAopApplication.java
@@ -0,0 +1,13 @@
+package com.re.fintechotpaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechOtpAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechOtpAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/re/fintechotpaop/FintechOtpAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re.fintechotpaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechOtpAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
